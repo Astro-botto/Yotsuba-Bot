a489a9b86d80176cfc25fbfda0567b11cbe4f6c8
Astro-botto <103898237+Astro-botto@users.noreply.github.com>
2022-05-06 04:28:47 -0700

    Add files via upload

--- src/lib/Mongo/Models/User.ts
+++ src/lib/Mongo/Models/User.ts
@@ -1,25 +1,86 @@
-import { Schema, model } from 'mongoose'
+import { Schema, model } from "mongoose";
 import { IUserModel } from "../../../typings";
 const UserSchema = new Schema({
-    jid: {
-        type: String,
-        required: true,
-        unique: true
+  jid: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  ban: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  warnings: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  Xp: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  wallet: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  bank: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  lastDaily: {
+    type: Number,
+  },
+  lastRob: {
+    type: Number,
+  },
+  pokemons: {
+    type: [String],
+  },
+  party: [
+    {
+      id: Number,
+      level: Number,
+      name: String,
+      image: String,
     },
-    ban: {
-        type: Boolean,
-        required: true,
-        default: false
+  ],
+  pc: [
+    {
+      id: Number,
+      level: Number,
+      name: String,
+      image: String,
     },
-    warnings: {
-        type: Number,
-        required: true,
-        default: 0
+  ],
+  haigusha: {
+    name: String,
+    id: Number,
+  },
+  married: {
+    type: Boolean,
+    default: false,
+    required: false,
+  },
+  lastQuizId: {
+    type: Number,
+  },
+  quizPoints: {
+    type: Number,
+    default: 0,
+    required: false,
+  },
+  gallery: [
+    {
+      id: Number,
+      name: String,
+      image: String,
+      about: String,
+      source: String,
     },
-    Xp: {
-        type: Number,
-        required: true,
-        default: 0
-    }
-})
-export default model<IUserModel>('users', UserSchema)
+  ],
+});
+export default model<IUserModel>("users", UserSchema);
